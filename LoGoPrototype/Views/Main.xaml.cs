da131c489d6fa663a406a4b028f354211ec60001
Robert <roberteriksson12@gmail.com>
2018-12-04 10:59:33 +0100

    first commit

---
+++ LoGoPrototype/Views/Main.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using LoGoPrototype;
+
+using Xamarin.Forms;
+
+namespace LoGoPrototype.Views
+{
+    public partial class Main : ContentPage
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
