7617db32226cbd27ad5472ebac2ef3602567a4fd
Robert <roberteriksson12@gmail.com>
2018-12-05 22:57:55 +0100

    Much added to codehandler. command done, minor changes to codeviewmodel

---
+++ LoGoPrototype/Models/CodeHandler.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace LoGoPrototype.Models
+{
+    public class CodeHandler
+    {
+        private int index;
+        private  string code;
+        public  string Code 
+        { 
+            get
+            {
+                return code;
+            }
+            set
+            {
+                code = value;
+                Console.WriteLine("...code in codehandler...");
+                Console.WriteLine(code);
+                //Console.WriteLine(Parse());
+            }
+        }
+
+        public CodeHandler(string code)
+        {
+            Code = code;
+            index = 0;
+        }
+
+        public List<Command> Parse()
+        {
+            List<Command> commands = new List<Command>();
+            Regex movement = new Regex(@"^([fb]d|[lr]t)$");
+            Regex pen = new Regex(@"^p");
+            Regex repeat = new Regex(@"^repeat$");
+            while (RemainingTokens())
+            {
+                string token = NextToken();
+                if (movement.Match(token).Success)
+                {
+                    Command cmd = new Command(token, NextToken());
+                    commands.Add(cmd);
+                } else if (pen.Match(token).Success)
+                {
+                    Command cmd = new Command(token);
+                    commands.Add(cmd);
+                } else if (repeat.Match(token).Success)
+                {
+                    Command cmd = new Command(token, NextToken());
+                    string toRepeat = GetRepeat();
+                    CodeHandler codeHandler = new CodeHandler(toRepeat);
+                    cmd.Commands = codeHandler.Parse();
+                    commands.Add(cmd);
+                }
+            }
+            foreach (Command cmd in commands)
+            {
+                Console.WriteLine(cmd.Action + " " + cmd.Amount);
+            }
+            return commands;
+        }
+
+        public string NextToken()
+        {
+            string token = "";
+            char c = code[index];
+
+            // If space, ignore
+            if (c.Equals(' '))
+            {
+                index++;
+                return NextToken();
+            }
+
+            // If bracket, send back
+            if (c.Equals('[') || c.Equals(']'))
+            {
+                index++;
+                return c.ToString();
+            }
+
+            // Otherwise, accumulate until a space
+            while (!c.Equals(' ') && RemainingTokens())
+            {
+                token += c;
+                c = code[++index];
+            }
+            return token;
+        }
+
+        public string GetRepeat()
+        {
+            while (!code[index].Equals('[') && RemainingTokens()) { }
+
+            int start = index;
+            int bracketCount = 1;
+
+            while (bracketCount > 0 && RemainingTokens())
+            {
+                char c = code[index++];
+                if (c.Equals('['))
+                {
+                    bracketCount++;
+                } else if (c.Equals(']'))
+                {
+                    bracketCount--;
+                }
+            }
+            int end = index;
+            return Code.Substring(start, end - 1);
+        }
+
+        private bool RemainingTokens()
+        {
+            return !code.Equals(null) && index < code.Length;
+        }
+    }
+}
---
+++ LoGoPrototype/Models/Command.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LoGoPrototype.Models
+{
+    public class Command
+    {
+        private List<Command> commands = new List<Command>();
+        public List<Command> Commands
+        {
+            get
+            {
+                return commands;
+            }
+            set
+            {
+                commands = value;
+            }
+        }
+        private string action;
+        public string Action
+        {
+            get
+            {
+                return action;
+            }
+            set
+            {
+                action = value;
+            }
+        }
+        public string Amount { get; set; }
+
+        public Command(string action)
+        {
+            this.action = action;
+        }
+
+        public Command(string action, string amount)
+        {
+            this.action = action;
+            this.Amount = amount;
+        }
+    }
+}
