a40b569ead8054ea5bcb1e6692d9eaace15635b1
kristofferknape <kristoffer.knape@gmail.com>
2018-12-06 12:37:11 +0100

    first commit in draw

---
+++ LoGoPrototype/Models/Turtle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using SkiaSharp;
+
+namespace LoGoPrototype.Models
+{
+    public class Turtle
+    {
+        public SKPaint StrokePaint()
+        {
+
+            SKPaint paint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.White,
+                StrokeWidth = 5,
+                StrokeCap = SKStrokeCap.Round,
+                IsAntialias = true
+        };
+            return paint;
+        }
+
+        public SKPaint StrokePaint20()
+        {
+
+            SKPaint paint = new SKPaint
+            {
+                Style = SKPaintStyle.Stroke,
+                Color = SKColors.White,
+                StrokeWidth = 20,
+                StrokeCap = SKStrokeCap.Round,
+                IsAntialias = true
+            };
+            return paint;
+        }
+    }
+}
